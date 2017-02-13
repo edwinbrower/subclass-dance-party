86be92f6ff732c90bc557ecd59738dd4862a0bfd
edwinbrower <edwin.brower@gmail.com>
2017-02-13 15:44:55 -0800

    Refactor makeDancer and makeBlinkyDancer (functional style) to Dancer and BlinkyDancer (pseudoclassical style)

--- src/dancer.js
+++ src/dancer.js
@@ -1,19 +1,14 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps) {
+var Dancer = function(top, left, timeBetweenSteps) {
 
-  var dancer = {};
+  // var dancer = {};
 
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
-
-  dancer.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
-
-  dancer.setPosition = function(top, left) {
+  this.$node = $('<span class="dancer"></span>');
+  this.setPosition(top, left);
+  
+  this.step(timeBetweenSteps);
+  /*dancer.setPosition = function(top, left) {
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     //
@@ -22,11 +17,28 @@ var makeDancer = function(top, left, timeBetweenSteps) {
       left: left
     };
     dancer.$node.css(styleSettings);
-  };
+  };*/
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
 
-  return dancer;
+  // return dancer;
+};
+
+Dancer.prototype.setPosition = function(top, left) {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+
+  this.$node.css(styleSettings);
+};
+
+Dancer.prototype.step = function(timeBetweenSteps) {
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  setTimeout(this.step.bind(this, timeBetweenSteps), timeBetweenSteps);
 };
